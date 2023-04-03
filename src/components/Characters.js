932d7ae3862e2d9244d388f8a4a2d0445380b29f
David Corney <david.corney@coveainsurance.co.uk>
2023-04-03 22:52:04 +0100

    feat: add pagination for characters

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,4 +1,5 @@
-import { Button, Card, Loader, Message } from 'semantic-ui-react'
+import {useState} from 'react';
+import { Button, Card, Loader, Message, Pagination } from 'semantic-ui-react'
 import { useGetCharactersQuery } from '../services/swapApi'
 import { nanoid } from '@reduxjs/toolkit'
 import { useDispatch } from 'react-redux'
@@ -6,7 +7,8 @@ import { addFave } from '../features/faves'
 import CharacterDetails from './CharacterDetails'
 
 const Characters = () => {
-	const { data, isError, isLoading } = useGetCharactersQuery();
+	const [activePage, setActivePage] = useState(1);
+	const { data, isError, isLoading } = useGetCharactersQuery(activePage);
 	const dispatch = useDispatch()
 
 	const selectCharacter = e => {
@@ -16,6 +18,10 @@ const Characters = () => {
 	}
 	const addToFavourites = e => dispatch(addFave(selectCharacter(e)))
 
+	const onChange = (e,pageInfo) => {
+		setActivePage(pageInfo.activePage);
+	}
+
 	if (isLoading) {
 		return <Loader active={isLoading} />
 	}
@@ -23,7 +29,9 @@ const Characters = () => {
 		return <Message error={isError}>There was an error</Message>
 	}
 	if (data && Boolean(data?.results?.length)) {
+		const numberOfPages = Math.floor(data.count/10);
 		return (
+			<>
 				<Card.Group centered>
 					{data.results.map(character => (
 						<Card key={nanoid()}>
@@ -43,6 +51,18 @@ const Characters = () => {
 						</Card>
 					))}
 				</Card.Group>
+				<div class="ui container center aligned">
+					<Pagination
+						defaultActivePage={1}
+						boundaryRange={0}
+						firstItem={null}
+						lastItem={null}
+						totalPages={numberOfPages}
+						ellipsisItem={null}
+						onPageChange={onChange}
+					/>
+				</div>
+			</>
 		)
 	} else if (data?.results?.length === 0) {
 		return <Message warning>no films found</Message>
