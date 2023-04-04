5b972f01335d722ade0be6f44c5b01fa219e5a7d
David Corney <david.corney@coveainsurance.co.uk>
2023-04-04 09:23:08 +0100

    refactor: fave item interface

--- src/components/Faves/types.ts
+++ src/components/Faves/types.ts
@@ -1,10 +1,11 @@
 import {SyntheticEvent} from 'react'
 import {ButtonProps, RatingProps} from 'semantic-ui-react'
 
-export interface FaveItemObj{
+export interface FaveItemObj {
     id:string;
     rating: number;
     name: string;
+    [key: string]: string | string[] | number;
 }
 
 export type handleRemove = (_e: SyntheticEvent, data: ButtonProps) => void
